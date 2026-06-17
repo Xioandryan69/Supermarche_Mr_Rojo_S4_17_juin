68bcff7d57e896b6110563a368f901a1ad59ff8a
Andrianandrainy Ny Avo <andriannyavo9@gmail.com>
2026-06-17 10:03:11 +0300

    ok

---
+++ app/Views/achats/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <select name="" id="" >
+        
+        <option value="">Produit</option>
+        <option value="">Produit</option>
+        <option value="">Produit</option>
+
+    
+
+        
+    </select>
+    
+    
+</body>
+</html>
+
+
+<!-- /*
+
+saisie achat ::
+
+
+selectionner le produit
+
+entrer quantite
+
+puis valider 
+
+
+Tableaux :
+
+Produit .Prix_Unitaire,Quantite ,Montant
+
+
+Produit 
+*/ -->
